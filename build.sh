8b7e1985941ebac959822e92064ee408028ba066
unknown <sravanorchu@gmail.com>
2026-06-23 10:52:10 +0530

    changes

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-pip install -r requirements-render.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py seed_hospital_data
